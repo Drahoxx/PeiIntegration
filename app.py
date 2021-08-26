1fb747e07b3bff79a9cb64d7a01d4fe44cb89729
Drahoxx <Drahoxx@users.noreply.github.com>
2021-08-26 15:47:16 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+import os 
 from data import Challenges
+from pathlib import Path
 
 app = Flask(__name__)
-
+app.config["IMAGE_UPLOADS"] = "/home/drahoxx/Documents/School/SiteIntePei1/uploads"
 challs = Challenges()
 
 @app.route("/")
@@ -26,6 +28,25 @@ def latest():
 def challenges():
 	return render_template('challenges.html',challenges=challs)
 
-@app.route("/challenges/<string:id>/")
+
+@app.route("/admin/")
+def admin():
+	return render_template('admin.html')
+
+@app.route("/challenges/<string:id>/", methods=['GET', 'POST'])
 def particularChallenge(id):
-	return render_template('challenge.html',id=id)
+	#Upload
+	if request.method == "POST":
+		if request.files:
+			image = request.files["image"]
+			Path(app.config["IMAGE_UPLOADS"]+f'/{id}').mkdir(parents=True, exist_ok=True)
+			image.save(os.path.join(app.config["IMAGE_UPLOADS"]+f'/{id}', image.filename))
+			print("Image saved")
+			return redirect(request.url)
+
+	c={"id":0,"title":"We got an error","desc":"Invalid id", "points":0}
+	for ch in challs:
+		if ch.get("id")==int(id):
+			c=ch
+			break
+	return render_template('challenge.html',c = c)
